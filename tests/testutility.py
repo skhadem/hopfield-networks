0c59334bba69c0c7434b2532dc6f6dae20740a49
nikhilrajaram <nira8877@colorado.edu>
2020-04-22 15:17:01 -0600

    allow size adjustment

--- tests/testutility.py
+++ tests/testutility.py
@@ -6,8 +6,8 @@ class TestUtility:
     SHAPES_PATH = "../data/MPEG7_CE-Shape-1_Part_B"
 
     @classmethod
-    def get_n_random_imgs_idb(cls, n):
+    def get_n_random_imgs_idb(cls, n, **kwargs):
         names = [_ for _ in os.listdir(cls.SHAPES_PATH) if _[-4:] == '.gif']
         random.shuffle(names)
         fpaths = ["{}/{}".format(cls.SHAPES_PATH, name) for name in names[:n]]
-        return InputDataBuilder(fpaths)
+        return InputDataBuilder(fpaths, **kwargs)
